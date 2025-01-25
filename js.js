1963f2e05638e4f0fb3bfe3b207093e34df8f74f
FREE FIRE <karthickraja291120@gmail.com>
2025-01-25 21:10:12 +0530

    Developer

--- js.js
+++ js.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Karthickraja","Developer"],
+    strings : ["Karthickraja"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
